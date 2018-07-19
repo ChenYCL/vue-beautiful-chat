c495ccaffc55c7f59a76e5fa72e65817e59d96b0
Marco Paoletti <marco.paoletti@ennova.it>
2018-07-19 17:35:10 +0200

    [#FIX] v-show for video publisher instead of v-if

--- src/ChatWindow.vue
+++ src/ChatWindow.vue
@@ -12,6 +12,7 @@
       :imageUrl="agentProfile.imageUrl"
       :chatImageUrl="agentProfile.imageUrl"
       :mainColor="mainColor"
+      v-show="!video"
     />
     <div id="video" class="sc-video" v-show="video"> 
         <div id="subscriber_video"></div>
